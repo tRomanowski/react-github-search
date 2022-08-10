866bcc53b901edb508cc64a3a12f2bdb925128ab
Tim Romanowski <timromanowski15@gmail.com>
2022-08-10 12:43:04 +0200

    Add error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -6,7 +6,8 @@ const Error = () => {
     <Wrapper>
       <div>
         <h1>404</h1>
-        <h3>Sorry page not found</h3>
+        <h3>sorry, the page you tried cannot be found</h3>
+        <Link to='/' />
       </div>
     </Wrapper>
   );
