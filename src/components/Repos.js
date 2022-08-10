47de18166e00c750065e6389d76672207e88e2b8
Tim Romanowski <timromanowski15@gmail.com>
2022-08-10 17:50:52 +0200

    Start making charts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,52 @@
 import React from 'react';
 import styled from 'styled-components';
-import { GithubContext } from '../context/context';
+import { useGlobalContext } from '../context/context';
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = useGlobalContext();
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) {
+      return total;
+    }
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
+  const chartData = [
+    {
+      label: 'html',
+      value: '13',
+    },
+    {
+      label: 'CSS',
+      value: '23',
+    },
+    {
+      label: 'Javascript',
+      value: '80',
+    },
+  ];
+  return (
+    <section className='section'>
+      <Wrapper className='section-center'>
+        {/* <ExampleChart data={chartData} /> */}
+        <Pie3D data={languages} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
